db95f93adcf86213d7ae28084428d8f67583a141
Suman Kumar <sumancsnit@gmail.com>
2025-12-15 19:07:19 +0530

    move files

--- src/features/account/AccountOperations.js
+++ src/features/account/AccountOperations.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deposit, payLoan, requestLoan, withdraw } from './accountSlice';
+import { deposit, payLoan, requestLoan, withdraw } from '../customers/accountSlice';
 
 const AccountOperations = () => {
   const dispatch = useDispatch();
